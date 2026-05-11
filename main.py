dd3d7c443f25de3c568251dc5ba410e18244961e
jasperntc <jasperntc@gmail.com>
2026-05-11 23:48:16 +0800

    Added player class and implemented rotate and update method

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,7 +21,11 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        time = clock.tick(60)
+        dt = time / 1000
 
 if __name__ == "__main__":
     main()
